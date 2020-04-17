0cbbb92a0e37664496f1dc06e693c83ea96d4d2f
lgdev07 <luan_saopaulo06@hotmail.com>
2020-04-16 22:33:41 -0300

    socket

--- memory.py
+++ memory.py
@@ -5,4 +5,11 @@ ref = 'Sarah ' * 51200
 time2 = tracemalloc.take_snapshot()
 stats = time2.compare_to(time1, 'lineno')
 for stat in stats[:4]:
-    print(stat)
+  pass
+    # print(stat)
+
+stats = time2.compare_to(time1, 'traceback')
+top = stats[0]
+
+print(stats[0])
+# print('\n'.join(top.traceback.format()))
